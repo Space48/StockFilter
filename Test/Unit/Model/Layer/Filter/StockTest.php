99e9b8d7e42a9338ea4e8ffc033ad443af176e4d
Adam Moss <adam@space48.com>
2017-11-30 11:50:37 +0000

    Removes test code

--- Test/Unit/Model/Layer/Filter/StockTest.php
+++ Test/Unit/Model/Layer/Filter/StockTest.php
@@ -4,9 +4,5 @@ namespace Space48\StockFilter\Model\Layer\Filter;
 
 class StockTest extends \PHPUnit\Framework\TestCase
 {
-    public function testSomething()
-    {
-        $help = true;
-        $this->assertTrue($help);
-    }
+
 }
